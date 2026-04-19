d194d58a2967f1f62433df13dcbb8a00216421a3
JanMalteRoehm <137523993+JanMalteRoehm@users.noreply.github.com>
2026-04-20 00:28:58 +0200

    Add files via upload

--- website home.js
+++ website home.js
@@ -1,27 +1,3 @@
-var indexValue = 1;
-showImg(indexValue);
-
-function side_slide(e) {
-    showImg(indexValue +=e); 
-}
-
-function showImg(e) {
-    var i;
-    const img = document.querySelectorAll('.pic');
-    if(e > img.length) {
-        indexValue = 1
-    }
-    if(e < 1) {
-        indexValue = img.length
-    }
-    for(i = 0; i < img.length; i++) {
-        img[i].style.display = "none";
-    }
-    img[indexValue - 1].style.display = "block";
-}
-
-
-
       /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
       var dropdown = document.getElementsByClassName("dropdown-btn");
       var i;
@@ -38,3 +14,5 @@ function showImg(e) {
         });
       }
    
+
+    
